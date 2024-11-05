87896d95117b198c1046b3734022913568ee6e83
ninja-con-gafas <absdarekar01@gmail.com>
2024-11-05 11:44:19 +0530

    feat: Updated schema of metadata table.
    - Added new fields in the table:
    	file_size (int):	The size of the video file in bytes.
    	video_duration (int):	The duration of the video in second.
    	file_id (str):		The file ID of the shared video.
    - Used feed date as date of entry in the metadata table.
    - Implemented ffmpeg.probe to extract video information.
    - Updated documentation.

--- process/downloader.py
+++ process/downloader.py
@@ -2,29 +2,19 @@
 The module downloads the YouTube videos to local video repository and handles management of video metadata including
 extracting information from transcripts and storing metadata in a SQLite database. It integrates with the Gemini 1.5
 Flash LLM for analyzing video transcripts and employs SQLAlchemy for database interactions.
-
-constants:
-    - DOWNLOAD_PATH: The path where downloaded videos will be stored, retrieved from the environment.
-    - GEMINI_DEVELOPER_API_KEY_PATH: The API key for accessing the Gemini API, retrieved from the environment.
-    - YOUTUBE_VIDEO_REPOSITORY_PATH: The path for the YouTube video repository, retrieved from the environment.
-
-dependencies:
-    - utilities.google.ai
-    - utilities.google.youtube
-    - pandas
-    - sqlalchemy
 """
 
-from datetime import date
+from ffmpeg import probe
 from google.ai import get_response
 from google.iam import read_api_key
 from google.youtube import download_video_as_mp4, get_video_id, get_video_transcript_en
 from os import environ, path
 from pandas import DataFrame, read_csv
-from sqlalchemy import Boolean, create_engine, Column, DateTime, MetaData, select, String, Table, update
+from re import search
+from sqlalchemy import Boolean, create_engine, Column, DateTime, Integer, MetaData, select, String, Table, update
 from sqlalchemy.engine.base import Engine
 from sqlalchemy.engine.row import Row
-from typing import Set, Sequence
+from typing import Dict, Set, Sequence, Any
 
 DOWNLOAD_PATH = environ.get("DOWNLOAD_PATH")
 GEMINI_DEVELOPER_API_KEY_PATH = read_api_key(environ.get("GEMINI_DEVELOPER_API_KEY_PATH"))
@@ -34,13 +24,16 @@ def create_metadata_database(engine: Engine) -> None:
 
     """
     Create a `metadata` database and the associated `metadata` table with the following columns:
-        date (DateTime):                            The date and time associated with the metadata entry.
+        date (DateTime):                            The feed date as date of record.
         url (String):                               The URL of the video.
         video_id (String, Primary Key):             The unique identifier for the video serving as a primary key.
         info (String):                              Additional information related to the video.
         is_downloaded (Boolean, default=False):     A flag indicating whether the video has been downloaded.
+        file_size (Integer, default=0):             The size of the video file in bytes.
+        video_duration (Integer, default=0):        The duration of the video in second.
         is_uploaded (Boolean, default=False):       A flag indicating whether the video has been uploaded.
-        shared_url (String, default=None):          A URL for sharing the video, if applicable.
+        shared_url (String, default=""):            The URL for sharing the video.
+        file_id (String, default=""):               The file ID of the shared video.
 
     args:
         engine (Engine): An instance of SQLAlchemy's Engine to connect to the database.
@@ -54,14 +47,17 @@ def create_metadata_database(engine: Engine) -> None:
 
     print("Creating database and table.")
     database_metadata: MetaData = MetaData()
-    Table('metadata', database_metadata,
-          Column('date', DateTime),
-                Column('url', String),
-                Column('video_id', String, primary_key=True),
-                Column('info', String),
-                Column('is_downloaded', Boolean, default=False),
-                Column('is_uploaded', Boolean, default=False),
-                Column('shared_url', String, default=None))
+    Table("metadata", database_metadata,
+          Column("date", DateTime),
+          Column("url", String),
+          Column("video_id", String, primary_key=True),
+          Column("info", String),
+          Column("is_downloaded", Boolean, default=False),
+          Column("file_size", Integer, default=0),
+          Column("video_duration", Integer, default=0),
+          Column("is_uploaded", Boolean, default=False),
+          Column("shared_url", String, default=""),
+          Column("file_id", String, default=""))
     database_metadata.create_all(bind=engine)
 
 def download_video(engine: Engine) -> None:
@@ -80,7 +76,7 @@ def download_video(engine: Engine) -> None:
         yt_dlp.utils.ExtractorError: If there is an error extracting the video information.
     """
 
-    metadata: Table = Table('metadata', MetaData(), autoload_with=engine)
+    metadata: Table = Table("metadata", MetaData(), autoload_with=engine)
     with engine.connect() as connection:
         rows: Sequence[Row] = connection.execute(select(metadata.c.url, metadata.c.video_id)
                                                  .where(metadata.c.is_downloaded == False)).fetchall()
@@ -96,10 +92,21 @@ def download_video(engine: Engine) -> None:
                                   file_name=video_id,
                                   url=video_url)
 
-            if path.exists(f"{DOWNLOAD_PATH}/{video_id}.mp4"):
+            video_file_path: str = f"{DOWNLOAD_PATH}/{video_id}.mp4"
+            if path.exists(video_file_path):
+                video_file_info: Dict[str, Any] = probe(filename=video_file_path,
+                                        v='error',
+                                        select_streams='v:0',
+                                        show_entries='stream=duration')
+
+                file_size: int = video_file_info.get("format").get("size")
+                video_duration: int = int(round(float(video_file_info.get("format").get("duration"))))
+
                 connection.execute((update(metadata)
                                     .where(metadata.c.video_id == video_id)
-                                    .values(is_downloaded=True)))
+                                    .values(is_downloaded=True,
+                                            file_size=file_size,
+                                            video_duration=video_duration)))
                 print(f"Downloaded the video {video_url} successfully.")
             else:
                 print(f"Failed to download the video {video_url}.")
@@ -113,7 +120,7 @@ def downloader(feed: str) -> None:
         feed (str): The file path to the CSV feed containing YouTube video URLs.
 
     returns:
-        bool: True if videos were successfully downloaded, False if no updates were made.
+        None
 
     raises:
         FileNotFoundError: If the specified feed CSV file is not found.
@@ -126,7 +133,8 @@ def downloader(feed: str) -> None:
     engine: Engine = create_engine(f"sqlite:///{YOUTUBE_VIDEO_REPOSITORY_PATH}metadata.db")
     if not path.exists(f"{YOUTUBE_VIDEO_REPOSITORY_PATH}metadata.db"):
         create_metadata_database(engine=engine)
-    update_metadata(engine=engine,
+    update_metadata(date=search(pattern=r"\d{4}-\d{2}-\d{2}", string=feed).group(0),
+                    engine=engine,
                     url=filter_duplicates(engine=engine,
                                           url=url.assign(video_id=lambda x: x["url"].apply(get_video_id))))
     download_video(engine=engine)
@@ -192,7 +200,7 @@ def filter_duplicates(engine: Engine, url: DataFrame) -> DataFrame:
     args:
         engine (Engine): An instance of SQLAlchemy's Engine to connect to the database.
         metadata (Table): The SQLAlchemy Table object representing the metadata table in the database.
-        url (DataFrame): A pandas DataFrame containing the columns 'url' and 'video_id'.
+        url (DataFrame): A pandas DataFrame containing the columns "url" and "video_id".
 
     returns:
         DataFrame: A DataFrame containing only the unique URLs that are not already present in the database.
@@ -201,33 +209,36 @@ def filter_duplicates(engine: Engine, url: DataFrame) -> DataFrame:
         SQLAlchemyError: If there is an error querying the database.
     """
 
-    metadata: Table = Table('metadata', MetaData(), autoload_with=engine)
+    metadata: Table = Table("metadata", MetaData(), autoload_with=engine)
     with engine.connect() as connection:
         existing_video_ids: Set[str] = {row[0] for row in connection.execute(select(metadata.c.video_id))}
-        duplicate_video_ids: DataFrame = url[url['video_id'].isin(existing_video_ids)]
+        duplicate_video_ids: DataFrame = url[url["video_id"].isin(existing_video_ids)]
         if not duplicate_video_ids.empty:
             print(f"Removed {len(duplicate_video_ids)} video_ids already present in the database:\n"
-                f"{duplicate_video_ids['video_id'].to_list()}")
+                f"{duplicate_video_ids["video_id"].to_list()}")
 
-    return url[~url['video_id'].isin(existing_video_ids)]
+    return url[~url["video_id"].isin(existing_video_ids)]
 
-def update_metadata(engine: Engine, url: DataFrame) -> None:
+def update_metadata(date: str, engine: Engine, url: DataFrame) -> None:
 
     """
-    Updates the 'metadata' table in the database by appending metadata of new YouTube video URLs. The metadata contains:
+    Updates the `metadata` table in the database by appending metadata of new YouTube video URLs. The metadata contains:
+        date (DateTime):            feed date as date of record.
         video_id (str):             extracted from each URL.
-        date (DateTime):            current date as date of record.
         info (str):                 video information extracted by processing the video transcript.
         is_downloaded (boolean):    default value False
+        file_size (int):            default value 0
+        video_duration (int):       default value 0
         is_uploaded (boolean):      default value False
-        shared (boolean):           default value None
+        shared_url (str):           default value ""
+        file_id (str):              default value ""
 
     args:
         engine (Engine): A SQLAlchemy engine object used to connect to the database.
-        url (DataFrame): A DataFrame containing an 'url' column of new YouTube video URLs.
+        url (DataFrame): A DataFrame containing an "url" column of new YouTube video URLs.
 
     returns:
-        bool: Indicates whether the metadata was updated, based on the availability of data.
+        None
     """
 
     if url.empty:
@@ -238,10 +249,19 @@ def update_metadata(engine: Engine, url: DataFrame) -> None:
     with engine.connect() as connection:
         (DataFrame({"url": url["url"],
                     "video_id": url["url"].apply(get_video_id)})
-        .assign(date=date.today().isoformat())
+        .assign(date=date)
         .assign(info=lambda x: (x["video_id"]
                                 .apply(get_video_transcript_en)
                                 .apply(extract_video_info)))
-        .assign(is_downloaded=False, is_uploaded=False, shared_url=None)
-        [['date', 'url', 'video_id', 'info', 'is_downloaded', 'is_uploaded', 'shared_url']]
+        .assign(is_downloaded=False, file_size=0, video_duration=0, is_uploaded=False, shared_url="", file_id="")
+        [["date",
+          "url",
+          "video_id",
+          "info",
+          "is_downloaded",
+          "file_size",
+          "video_duration",
+          "is_uploaded",
+          "shared_url",
+          "file_id"]]
          .to_sql(name="metadata", con=connection, if_exists="append", index=False, chunksize=1000))
